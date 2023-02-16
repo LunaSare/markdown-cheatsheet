739a4b4426a5572564894571e2a1fcac50486887
AviKaur4 <avineetkaurchahal@gmail.com>
2023-02-15 16:15:43 -0800

    adding file to remote repository

---
+++ Avi--markdown-cheatsheet.R
@@ -0,0 +1,30 @@
+A markdown cheatsheet file:
+  To create headers you use a hashtage symbol at the beginning of a line. 
+To add bold text you add ** ** before and after a word.
+To add italics you would enter as *word* with just one of each *.
+To add a blockquote you would use two quotation marks ("any words")
+To create an ordered list, you would simply use 1., 2. 3., etc on seperate lines.
+To create an unordered list, you would use dashes (-) on seperate lines.
+To format some code you would use 'some code', so use single quotation marks.
+To add a horizontal rule then you use these dashes (---)
+When you want to insert an external link, you would use markdown [guide](https://www.markdownguide.org/cheat-sheet/).
+To link to sections within a document you would jump to [header 2](#header-2)
+To insert images utilizing their URL, this can be used as a formatting exmaple: ![GitHub's mascot](https://myoctocat.com/assets/images/base-octocat.svg). So you add a ! before you write the rest of the code.
+To make a xkcd comic you would use the code <img src="https://api.time.com/wp-content/uploads/2014/08/xkcd-randll-munroe.jpg?quality=85&w=1600" width="500"> . We would add a <img src = before the link.
+To format a table, you would use dashes to seperate each heading label for the table, so each table should be seperated by the labels and leave an empty box if you want to fill the table in.
+To format a fenced code block, you would use ```{  and }``` to close it and use "": to seperate each label for example you would write as so ```
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+```
+To add a footnote, you would utilize [^1]:
+To create a definition list you would utilize **term 1** or you want to add **term 2** for another defintion. 
+To add a strikthrough text, you would use ~~some word~~ before any word.
+To create a task list, you would use - [] some task and you want the task to be checked off you would add an "x" -[x] some task.
+To insert emojis. you can type in :joy: to a generate a funny emoji.
+To highlight some text you would use ***some words*** you would use *** at the start and end of the sentence.
+To create a subscript, you can use this an example H<sub>2</sub>O so use <sub>/sub>
+To create a subscript you would use sup>some number/sup> for example you can use X<sup>2</sup>...
+############
